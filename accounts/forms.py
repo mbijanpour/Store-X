b7f7bac7dfe3db4f84bd321ca9badd477a7c40af
mbijanpour <mahanbijanpour@gmail.com>
2023-07-07 18:38:37 +0330

    registerUser forms Error handling

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,3 +10,12 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
+    
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if confirm_password != password:
+            raise forms.ValidationError('Passwords do not match')
+        
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 
 def registerUser(request):
@@ -15,11 +16,41 @@ def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
+            """
+            create a new user by using the form data directly
+            """
             # we set commit to false so we can change the role of the user and then save
-            user = form.save(commit=False)
+            # password = form.cleaned_data.get('password')
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.COSTUMER
+            # user.save()
+            
+            """
+            create a new user by using create_user method from the User model
+            """
+            first_name = form.cleaned_data.get('first_name')
+            last_name  = form.cleaned_data.get('last_name')
+            username   = form.cleaned_data.get('username')
+            email      = form.cleaned_data.get('email')
+            
+            password = form.cleaned_data.get('password')
+            
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
+            
             user.role = User.COSTUMER
-            user.save()
+            
+            user.save()            
+            
             return redirect('registerUser')
+        else:
+            print(f"form: {form.errors}")
     else:
         form = UserForm()
     context = {'form': form}
